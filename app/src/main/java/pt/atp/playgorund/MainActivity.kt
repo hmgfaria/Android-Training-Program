2ad654584778169e7fcf016764d4adeea300ab85
Helena Faria <hmgfaria@gmail.com>
2020-10-28 19:36:44 +0000

    clean main activity

--- app/src/main/java/pt/atp/playgorund/MainActivity.kt
+++ app/src/main/java/pt/atp/playgorund/MainActivity.kt
@@ -1,11 +1,18 @@
 package pt.atp.playgorund
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+
+        }
     }
+
+
 }
