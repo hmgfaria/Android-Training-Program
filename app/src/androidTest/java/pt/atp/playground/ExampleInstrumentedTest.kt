6d20893703d1fbe6bfde9b3550b81eb37a6e203a
Helena Faria <hmgfaria@gmail.com>
2020-11-11 16:02:15 +0000

    Login activity introduced, camera feature added, toast and snack bar

--- app/src/androidTest/java/pt/atp/playground/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/pt/atp/playground/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package pt.atp.playgorund
+package pt.atp.playground
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
---
+++ app/src/main/java/pt/atp/playground/DetailsActivity.kt
@@ -0,0 +1,13 @@
+package pt.atp.playground
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
